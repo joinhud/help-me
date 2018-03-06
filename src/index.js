98ef8f5c43dfa02c56b00f22306abfd0fc27a268
Yauheni Dubouski <joinhud@gmail.com>
2018-03-06 23:26:41 +0300

    help-me/feature: count how many k for not huge N

--- src/index.js
+++ src/index.js
@@ -1,3 +1,84 @@
 module.exports = function count(s, pairs) {
   // your implementation
+  const mod = 1000000007;
+  let count = 0;
+
+  if (!checkPairs(pairs)) {
+    return count;
+  }
+
+  let n = defineN(pairs);
+
+  if (n > 100000000) {
+    return count;
+  }
+
+  for (let i = 0; i <= n; i++) {
+    let conditionResult = true;
+
+    for (let j = 0, length = s.length; j < length; j++) {
+      let condition;
+
+      if (+s[j] === 0) {
+        condition = checkFirstCondition(n, j, i);
+      } else {
+        condition = checkSecondCondition(n, j, i);
+      }
+
+      conditionResult = conditionResult && condition;
+    }
+
+    if (conditionResult) {
+      count++;
+    }
+  }
+  
+  return count % mod;
+}
+
+function checkPairs(pairs) {
+  for (let i = 0, length = pairs.length; i < length; i++) {
+    if (pairs[i][1] > 100000000) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+function defineN(pairs) {
+  let result = 1;
+
+  for (let i = 0, length = pairs.length; i < length; i++) {
+    result = (result * (pairs[i][0] ** pairs[i][1]));
+  }
+
+  return result;
+}
+
+function checkFirstCondition(n, j, k) {
+  let divisor = gcd(k + j, n);
+
+  return divisor > 0 && divisor !== 1;
+}
+
+function checkSecondCondition(n, j, k) {
+  let divisor = gcd(k + j, n);
+
+  return divisor > 0 && divisor === 1;
+}
+function gcd(a, b) {
+  if (a === 0 || b === 0) {
+    return -1;
+  }
+
+  while (a !== 0 && b !== 0) {
+    if (a > b) {
+      a = a % b;
+    } else {
+      b = b % a;
+    }
+  }
+
+  return a + b;
 }
